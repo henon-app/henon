f71d87701b7ff96c9b5008f13e9ae221dd0f489b
Henon <asaminewpio60@gmail.com>
2026-05-10 18:09:07 +0300

    fix supabase key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://wpzulvmydnxplntuwgol.supabase.co';
-const supabaseKey = 'sb_publishable_unG6XlyvocRYPnRh4027vA_1Kf38';
+const supabaseKey = 'sb_publishable_unG6XlyvocRYPnRh4O27vA_1Kf38bhi';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
